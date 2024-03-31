282e51a8f2608eb02ac50780cfae8c8ffa4bb7cb
ghofrane <ghofrane.ghanmi01@gmail.com>
2024-04-01 00:11:19 +0100

    nearly ready to check

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// document.body.style.cursor = 'none';
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
